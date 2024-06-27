01c99361ae409e34699c37d48c8a1696bde21bfb
Nadia Pisarello <pisarello.nads@gmail.com>
2024-06-26 21:46:59 -0300

    "feat: implement dotenv and enhance DB connection setup"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { TurnosModule } from './turnos/turnos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsuariosModule } from './usuarios/usuarios.module';
 
+const URL_CONNECT = process.env.URL_CONNECT
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/centrodb'),
+
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        try {
+          return {
+            uri: URL_CONNECT,
+            useUnifiedTopology: true,
+          };
+        } catch (err) {
+          console.error('Error connecting to MongoDB ', err)
+          throw err
+        };
+      }
+    }),
     TurnosModule,
     UsuariosModule],
 
