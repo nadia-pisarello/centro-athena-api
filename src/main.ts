421562431d8d7c6d0c201d75fac7d1cd8cef685a
Nadia Pisarello <pisarello.nads@gmail.com>
2024-06-26 22:22:19 -0300

    fix: resolve err

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  app.setGlobalPrefix('api')
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  const port = process.env.PORT || 3000
-  await app.listen(port);
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
