ca36cc90c66372350b75bdbad90198ef036d6f1c
Nadia Pisarello <pisarello.nads@gmail.com>
2024-08-30 13:11:08 -0300

    refactor modulo usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TurnosModule } from './turnos/turnos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsuariosModule } from './usuarios/usuarios.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    // MongooseModule.forRoot('mongodb://localhost:27017/centrodb'),
     MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.4sheukv.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority&appName=CentroAthena`),
     TurnosModule,
-    UsuariosModule],
-
+    UsuariosModule,
+  ]
 })
 export class AppModule { }
