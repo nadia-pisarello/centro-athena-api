3a1e9938f4175cae279e782321e3036f4a284466
Nadia Pisarello <pisarello.nads@gmail.com>
2024-08-30 16:27:26 -0300

    feat: vercel file

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
