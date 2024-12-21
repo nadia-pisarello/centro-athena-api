8d72c8e5dbd6709d57c1733aaf078db128f02624
Nadia Pisarello <pisarello.nads@gmail.com>
2024-12-21 16:14:15 -0300

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
